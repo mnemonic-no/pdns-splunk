8f1a70f111e0966ebab07013e4700d07fc257006
Fredrik Borg <fredrikb@mnemonic.no>
2018-09-18 21:34:55 +0200

    rename to mpdns

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-version=$(grep -oP '(?<=version=).*' pdns/default/app.conf)
-filename=dist/pdns-${version}.spl
+version=$(grep -oP '(?<=version=).*' mpdns/default/app.conf)
+filename=dist/mpdns-${version}.spl
 mkdir -p dist
 rm -f $filename
 
 # Update documentation from README.md
-cat README.md | sed -e 's/\!\[.*\?\]\(.*\?\)//g' | pandoc -t plain | sed '/^$/N;/^\n$/D' > pdns/README
+cat README.md | sed -e 's/\!\[.*\?\]\(.*\?\)//g' | pandoc -t plain | sed '/^$/N;/^\n$/D' > mpdns/README
 
 # Exclude files that will make splunk-appinspect fail
 tar zcf $filename \
     --exclude-vcs-ignores \
     --exclude=./build.sh \
-    --exclude=pdns/local \
-    --exclude=pdns/metadata/local.meta \
+    --exclude=mpdns/local \
+    --exclude=mpdns/metadata/local.meta \
     --exclude=__pycache__ \
     --exclude=act/bin/lib/chardet/docs/Makefile \
     --exclude=.git \
     --exclude=\*.pyc \
     --exclude=.gitignore \
     --exclude=.gitmodules \
-    pdns
+    mpdns
 
 splunk-appinspect inspect $filename
